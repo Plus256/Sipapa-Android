930cf3060e9eb70c4be29c1cdc4130c4e0a339d8
Collins Wagaba <wagabaplus256@gmail.com>
2015-07-14 06:48:02 -0700

    tweaking the UI

--- gen/com/plus256/sipapa/R.java
+++ gen/com/plus256/sipapa/R.java
@@ -11,7 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int ThemeColor=0x7f040000;
+        public static final int Black=0x7f040002;
+        public static final int ChocoBrown=0x7f040009;
+        public static final int CoolBlue=0x7f040008;
+        public static final int DarkGrey=0x7f040003;
+        public static final int DarkOrange=0x7f040007;
+        public static final int Green=0x7f040000;
+        public static final int LightGreen=0x7f040005;
+        public static final int LightGrey=0x7f040004;
+        public static final int ThemeColor=0x7f040006;
         public static final int White=0x7f040001;
     }
     public static final class dimen {
@@ -24,51 +32,95 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f050003;
+        public static final int activity_horizontal_margin=0x7f050005;
         public static final int activity_rare_space=0x7f050001;
+        public static final int activity_vertical_margin=0x7f050006;
+        public static final int basic_thumbnail=0x7f050004;
+        public static final int big_divider=0x7f050003;
         public static final int tiny_divider=0x7f050002;
     }
     public static final class drawable {
         public static final int delhi_rickshaw=0x7f020000;
-        public static final int facebook_delete=0x7f020001;
-        public static final int gaming=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int internet_comm=0x7f020004;
-        public static final int lg_g4=0x7f020005;
-        public static final int new_macbook=0x7f020006;
-        public static final int project_spartan=0x7f020007;
-        public static final int tag_watch=0x7f020008;
-        public static final int top_cover=0x7f020009;
-        public static final int visual_studio=0x7f02000a;
+        public static final int facebook=0x7f020001;
+        public static final int facebook_delete=0x7f020002;
+        public static final int gaming=0x7f020003;
+        public static final int google=0x7f020004;
+        public static final int ic_chevron_left=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int ic_like=0x7f020007;
+        public static final int ic_menu=0x7f020008;
+        public static final int ic_more_vert=0x7f020009;
+        public static final int ic_search=0x7f02000a;
+        public static final int ic_share=0x7f02000b;
+        public static final int internet_comm=0x7f02000c;
+        public static final int lg_g4=0x7f02000d;
+        public static final int logo=0x7f02000e;
+        public static final int new_macbook=0x7f02000f;
+        public static final int project_spartan=0x7f020010;
+        public static final int rafiki=0x7f020011;
+        public static final int tag_watch=0x7f020012;
+        public static final int top_cover=0x7f020013;
+        public static final int twitter=0x7f020014;
+        public static final int visual_studio=0x7f020015;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int article_row=0x7f090004;
-        public static final int main_article_container=0x7f090002;
-        public static final int subscribe_email=0x7f090003;
-        public static final int top_article=0x7f090000;
-        public static final int top_article_cover=0x7f090001;
+        public static final int about_text=0x7f090010;
+        public static final int action_settings=0x7f090013;
+        public static final int ad_container=0x7f090005;
+        public static final int ad_item=0x7f090007;
+        public static final int advertise_text=0x7f090012;
+        public static final int article_details=0x7f090008;
+        public static final int article_image=0x7f09000a;
+        public static final int article_row=0x7f090009;
+        public static final int article_title=0x7f09000b;
+        public static final int body_wrapper=0x7f090000;
+        public static final int contact_text=0x7f090011;
+        public static final int facebook_connect=0x7f09000d;
+        public static final int footer=0x7f09000c;
+        public static final int google_plus_connect=0x7f09000f;
+        public static final int main_article_container=0x7f090004;
+        public static final int subscribe_email=0x7f090006;
+        public static final int top_article=0x7f090001;
+        public static final int top_article_cover=0x7f090002;
+        public static final int top_article_title=0x7f090003;
+        public static final int twitter_connect=0x7f09000e;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int article_details=0x7f030001;
-        public static final int article_row=0x7f030002;
+        public static final int ad_item=0x7f030001;
+        public static final int article_details=0x7f030002;
+        public static final int article_row=0x7f030003;
+        public static final int footer=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int fetch_article_details=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060002;
+        public static final int about_text=0x7f060013;
+        public static final int action_more=0x7f06000f;
+        public static final int action_search=0x7f06000e;
+        public static final int action_settings=0x7f060003;
+        public static final int ad_description=0x7f060011;
+        public static final int ad_title=0x7f060010;
+        public static final int advertise_text=0x7f060015;
+        public static final int app_full_name=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int app_tagline=0x7f060001;
-        public static final int article_author=0x7f060006;
-        public static final int article_date=0x7f060007;
-        public static final int subscribe_button_text=0x7f06000a;
-        public static final int subscribe_email_hint=0x7f060009;
-        public static final int subscribe_head_title=0x7f060008;
-        public static final int top_article_cat=0x7f060004;
-        public static final int top_article_title=0x7f060005;
-        public static final int top_cover_descr=0x7f060003;
+        public static final int app_tagline=0x7f060002;
+        public static final int article_author=0x7f060007;
+        public static final int article_body=0x7f06000a;
+        public static final int article_date=0x7f060008;
+        public static final int article_views=0x7f060009;
+        public static final int contact_text=0x7f060014;
+        public static final int copyright=0x7f060012;
+        public static final int hello_world=0x7f060017;
+        public static final int social_link=0x7f060016;
+        public static final int subscribe_button_text=0x7f06000d;
+        public static final int subscribe_email_hint=0x7f06000c;
+        public static final int subscribe_head_title=0x7f06000b;
+        public static final int top_article_cat=0x7f060005;
+        public static final int top_article_title=0x7f060006;
+        public static final int top_cover_descr=0x7f060004;
     }
     public static final class style {
         /** 
--- src/com/plus256/sipapa/MainActivity.java
+++ src/com/plus256/sipapa/MainActivity.java
@@ -2,10 +2,16 @@ package com.plus256.sipapa;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -15,13 +21,92 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        //adding articles to their container
         LinearLayout main_article_container=(LinearLayout) findViewById(R.id.main_article_container);
         LinearLayout article_row=(LinearLayout) findViewById(R.id.article_row);
         
         LayoutInflater row_inflater=(LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View inflated_article_row=row_inflater.inflate(R.layout.article_row, article_row);
         
-        main_article_container.addView(inflated_article_row); //Thank you Jesus
+        main_article_container.addView(inflated_article_row); //Thank you JESUS
+        
+        //adding adverts to their container
+	    LinearLayout ad_container=(LinearLayout) findViewById(R.id.ad_container);
+	    LinearLayout ad_item=(LinearLayout) findViewById(R.id.ad_item);
+	    
+	    LayoutInflater ad_inflater=(LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	    View inflated_ad_item=ad_inflater.inflate(R.layout.ad_item, ad_item);
+	    
+	    ad_container.addView(inflated_ad_item);
+	    
+	  //setting top_article_cover onclicklistener
+	    ImageView top_article_cover=(ImageView) findViewById(R.id.top_article_cover);
+	    top_article_cover.setOnClickListener(new ImageView.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//String click_check="Hey";
+				//Toast.makeText(getBaseContext(), click_check, Toast.LENGTH_SHORT).show();
+				Intent article_details=new Intent(getBaseContext(), FetchArticleDetails.class);
+		    	startActivity(article_details);
+			}
+	    	
+	    });
+	    
+	  //setting top_article_title onclicklistener
+	    TextView top_article_title=(TextView) findViewById(R.id.top_article_title);
+	    top_article_title.setOnClickListener(new TextView.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//String click_check="Hey";
+				//Toast.makeText(getBaseContext(), click_check, Toast.LENGTH_SHORT).show();
+				Intent article_details=new Intent(getBaseContext(), FetchArticleDetails.class);
+		    	startActivity(article_details);
+			}
+	    	
+	    });
+	    
+	    //setting article_image onclicklistener
+	    ImageView article_image=(ImageView) findViewById(R.id.article_image);
+	    article_image.setOnClickListener(new ImageView.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//String click_check="Hey";
+				//Toast.makeText(getBaseContext(), click_check, Toast.LENGTH_SHORT).show();
+				Intent article_details=new Intent(getBaseContext(), FetchArticleDetails.class);
+		    	startActivity(article_details);
+			}
+	    	
+	    });
+	    
+	  //setting article_title onclicklistener
+	    TextView article_title=(TextView) findViewById(R.id.article_title);
+	    article_title.setOnClickListener(new TextView.OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				//String click_check="Hey";
+				//Toast.makeText(getBaseContext(), click_check, Toast.LENGTH_SHORT).show();
+				Intent article_details=new Intent(getBaseContext(), FetchArticleDetails.class);
+		    	startActivity(article_details);
+			}
+	    	
+	    });
         
     }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	return super.onOptionsItemSelected(item);
+    }
+    
 }
