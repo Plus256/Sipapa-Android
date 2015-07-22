00b7ac5850dbab7c2725018915563929fd5dd9b3
Collins Wagaba <wagabaplus256@gmail.com>
2015-07-22 10:55:03 +0300

    presentation ready

---
+++ app/src/main/java/com/plus256/sipapa/FetchArticleDetails.java
@@ -0,0 +1,15 @@
+package com.plus256.sipapa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FetchArticleDetails extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.article_details);
+	    
+	}
+}
